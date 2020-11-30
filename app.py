779c674e98a210e9f368439c15888395b9931efd
Varun Sharma <varun_sharma@live.co.uk>
2020-11-29 16:22:36 -0800

    backend python app.py cleanup

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ import translate, sentiment, synthesize
 app = Flask(__name__, static_url_path='/static')
 app.config['JSON_AS_ASCII'] = False
 
+# --------------------- Main Routes ------------------------------------------
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
@@ -31,97 +33,9 @@ def about():
 def test_widget():
     return render_template("test_widget.html", title="test_widget")
 
-@app.route("/rebit_1")
-def rebit_1():
-    return render_template("rebit_1.html", title="audio_widget")
-
-
-@app.route('/rebit_2')
-def rebit_2():
-    return render_template('rebit_2.html')
-
-
-@app.route('/rebit_3')
-def rebit_3():
-    return render_template('rebit_3.html')
-
-@app.route('/rebit_4')
-def rebit_4():
-    return render_template('rebit_4.html')
-
-@app.route('/rebit_5')
-def rebit_5():
-    return render_template('rebit_5.html')
-
-@app.route('/rebit_6')
-def rebit_6():
-    return render_template('rebit_6.html')
-
 @app.route('/rebit_cyberpulse_nov')
 def rebit_cyberpulse_nov():
     return render_template('rebit_cyberpulse_nov.html')
-
-
-# from flask_mail import Mail, Message
-
-# mail = Mail()
-# SECRET_KEY = os.getenv("SECRET_KEY")
-# app.secret_key = SECRET_KEY
-# MAIL_SERVER = os.getenv("MAIL_SERVER")
-# MAIL_PORT = os.getenv("MAIL_PORT")
-# MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-# MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-
-# app.config["MAIL_SERVER"] = MAIL_SERVER
-# app.config["MAIL_PORT"] = MAIL_PORT
-# app.config["MAIL_USE_SSL"] = True
-# app.config["MAIL_USERNAME"] = MAIL_USERNAME
-# app.config["MAIL_PASSWORD"] = MAIL_PASSWORD
-
-# mail.init_app(app)
-# ---------------------------------------Contact----------------------------------
-# @app.route('/contact', methods=['GET', 'POST'])
-# def contact():
-#   form = ContactForm()
- 
-#   if request.method == 'POST':
-#     if form.validate() == False:
-#       flash('All fields are required.')
-#       return render_template('contact.html', form=form)
-#     else:
-#       msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
-#       msg.body = """
-#       From: %s &lt;%s&gt;
-#       %s
-#       """ % (form.name.data, form.email.data, form.message.data)
-#       mail.send(msg)
- 
-#       return render_template('contact.html', success=True)
- 
-#   elif request.method == 'GET':
-#     return render_template('contact.html', form=form)
-
-# from wtforms import validators
-# from wtforms import Form, TextAreaField, SubmitField, validators, TextField, BooleanField
-# from wtforms.validators import Required, Email
-# class ContactForm(Form):
-#     name = TextField("Name",  [validators.Required("Please enter your name.")])
-#     email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
-#     subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
-#     message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
-#     submit = SubmitField("Send")
-# #------------------------------------Feedback-----------------------------------------
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, TextField, SubmitField
-# from wtforms.validators import DataRequired, Length
-# from forms import ContactForm
-# @app.route('/feedback', methods=('GET', 'POST'))
-# def feedback():
-#     form = ContactForm()
-#     if form.validate_on_submit():
-#         return redirect(url_for('success'))
-#     return render_template('feedback.html', form=form)
-
 
 # --------------------- Azure Cognitive Services ------------------------------------------
 @app.route('/azureservices')
@@ -159,7 +73,7 @@ def text_to_speech():
 
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     from waitress import serve
     # Turn debug on during local development mode
     port = int(os.environ.get('PORT', 33507))
